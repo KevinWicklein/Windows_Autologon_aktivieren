f167271c606203900e8e1a46380f012f10d460e6
Kevin Wicklein <162309115+KevinWicklein@users.noreply.github.com>
2024-06-06 15:15:29 +0200

    Update START.bat

--- START.bat
+++ START.bat
@@ -1,2 +1,2 @@
 @echo off
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "".\administrative_Freigaben_anpassen.ps1""' -Verb RunAs}"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "".\Autologon aktivieren.ps1""' -Verb RunAs}"
